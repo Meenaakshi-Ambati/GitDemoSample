32459cf70871e050c61ceaf3965dfbafe1dd753b
Meenaakshi-Ambati <ambatimeenakshi9@gmail.com>
2021-08-25 07:42:22 +0530

    Branch2 updated code

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace GitDemoSample
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Branch1 code Updated");
+            Console.WriteLine("Branch2 code Updated");
+
         }
     }
 }
